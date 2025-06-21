538d657850827cc1a3a43bfefd9bb025d5fa9eb6
Kyrylo_Kud <kudkiril@gmail.com>
2025-06-21 19:03:06 +0200

    changed

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-const start = 20;
-const end = 30;
-const list = Array.from({ length: end - start + 1}, (_, i) => start + i);
-// array.from - створює масив;
-// 30 - 20 + 1 = 11 кількість чисел; 
-// _ - позначка що не має значення для усування undefined бо створюється новий масив
-
-const extraList = list.flatMap((n, i) =>  // проходиться по елементам одночасно отримуючи індекс
-i === list.length - 1 
-? [n] : [n, n + 0.5]). join(' '); // на останньому елементі не додається 0.5 
-
-console.log(extraList);
+for (let i = 20; i <= 30; i += 0.5) {
+    // ініціалізація, умова, дія
+    console.log(i);
+}
